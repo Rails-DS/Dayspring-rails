5b09c3f2eb500cc9d4fb39a5be33541084780f08
shibu-rails <shibudimitri1699@gmail.com>
2022-08-30 15:32:26 +0530

    Edit Functionality added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,8 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
+# Creating new Portfolio
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -20,6 +22,25 @@ class PortfoliosController < ApplicationController
     end
   end
 
+#  Editing the portfolio 
 
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+
+# Updatig portfolio 
+
+  def update
+  	 @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path(@blog), notice: "Blog was successfully updated." }
+       else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 
 end
