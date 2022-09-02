c57f73a8409a0f66669594a78aec4e9dc085a3fb
shibu-rails <shibudimitri1699@gmail.com>
2022-09-02 12:05:58 +0530

    Default values bofore initializing data for portfolio-items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,6 @@ class Portfolio < ApplicationRecord
 	extend FriendlyId
   	friendly_id :title, use: :slugged
 
-  	# validates :title, :body, :main_image, :thumb_image, presence: true
-
   	#Custom Query to get a specific item in blogs 
   	def self.angular
   		where(subtitle: 'Angular')
@@ -11,4 +9,11 @@ class Portfolio < ApplicationRecord
 
   	#Another method to make cutsom Query 
   	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') } 
+
+  	after_initialize :set_defaults
+
+  	def set_defaults
+  		self.main_image ||= "https://place-hold.it/600x400/666/fff"
+  		self.thumb_image ||= "https://place-hold.it/350x200/666/fff"
+  	end
 end
