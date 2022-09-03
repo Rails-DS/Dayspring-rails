ec7031cc62e93b0e4658682e0fd033b64dbe68f4
shibu-DS <shibudimitri1699@gmail.com>
2022-09-03 12:53:38 +0530

    Added Controller concers for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
 
-# run configure_permitted_paramaters method if only recive devise controller communication 
-before_action :configure_permitted_parameters, if: :devise_controller?
-
-
-def configure_permitted_parameters
-	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-end
-
+	#using concerns	
+	include DeviseWhitelist
+	
 end
