00e9c113b4c584387490ed7ccc23948cb1e3b50f
shibu-DS <shibudimitri1699@gmail.com>
2022-09-03 13:27:59 +0530

    application conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
 
-<<<<<<< HEAD
 	#using concerns 	
-=======
-	#using concerns	
->>>>>>> ec7031cc62e93b0e4658682e0fd033b64dbe68f4
+	
+
 	include DeviseWhitelist
 	
 end
